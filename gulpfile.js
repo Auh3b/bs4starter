14419be26bdd9dce685a89e9e55932402118f098
RobertChiko <57346805+Auh3b@users.noreply.github.com>
2021-03-16 15:39:50 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,6 @@ const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
 
-// Compile SASS
-// gulp.task("sass", function () {
-//   return gulp
-//     .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
-//     .pipe(sass())
-//     .pipe(gulp.dest("src/css"))
-//     .pipe(browserSync.stream());
-// });
 function sass1() {
   return gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
@@ -18,17 +10,6 @@ function sass1() {
     .pipe(browserSync.stream());
 }
 
-// Move JS Files to SRC
-// gulp.task("js", function () {
-//   return gulp
-//     .src([
-//       "node_modules/bootstrap/dist/js/bootstrap.min.js",
-//       "node_modules/jquery/dist/jquery.min.js",
-//       "node_modules/tether/dist/js/tether.min.js",
-//     ])
-//     .pipe(gulp.dest("src/js"))
-//     .pipe(browserSync.stream());
-// });
 function js() {
   return gulp
     .src([
@@ -40,18 +21,6 @@ function js() {
     .pipe(browserSync.stream());
 }
 
-// Watch SASS & Serve
-// gulp.task("serve", ["sass"], function () {
-//   browserSync.init({
-//     server: "./src",
-//   });
-
-//   gulp.watch(
-//     ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
-//     ["sass"]
-//   );
-//   gulp.watch("src/*.html").on("change", browserSync.reload);
-// });
 function serve() {
   browserSync.init({
     server: {
@@ -66,36 +35,12 @@ function serve() {
   gulp.watch("src/*.html").on("change", browserSync.reload);
 }
 
-// function watchFiles() {
-//   gulp
-//     .watch(
-//       ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
-//       ["sass"]
-//     )
-//     .on("change", browserSync.reload);
-//   gulp.watch("src/*.html").on("change", browserSync.reload);
-// }
-
-// const watching = gulp.parallel(serve, watchFiles);
-
-// Move Font Awesome Fonts folder to src
-// gulp.task("fonts", function () {
-//   return gulp
-//     .src("node_modules/font-awesome/fonts/*")
-//     .pipe(gulp.dest("src/fonts"));
-// });
 function fonts() {
   return gulp
     .src("node_modules/font-awesome/fonts/*")
     .pipe(gulp.dest("src/fonts"));
 }
 
-// Move font awesome css file
-// gulp.task("fa", function () {
-//   return gulp
-//     .src("node_modules/font-awesome/css/font-awesome.min.css")
-//     .pipe(gulp.dest("src/css"));
-// });
 function fa() {
   return gulp
     .src("node_modules/font-awesome/css/font-awesome.min.css")
